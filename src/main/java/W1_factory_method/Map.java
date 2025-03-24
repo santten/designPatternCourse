8b0d95f2e47ed6efb83d0f348ec35cccb1571277
santten <santtuss@metropolia.fi>
2025-03-24 22:57:42 +0200

    first commit week 1 hw

---
+++ src/main/java/W1_factory_method/Map.java
@@ -0,0 +1,9 @@
+package W1_factory_method;
+
+public abstract class Map {
+    public abstract Tile createTile();
+
+    public void display() {
+
+    };
+}
---
+++ src/main/java/W1_factory_method/Tile.java
@@ -0,0 +1,10 @@
+package W1_factory_method;
+
+public abstract class Tile {
+    public abstract String getCharacter();
+    public abstract String getType();
+
+    public void action(){
+        System.out.println("This is a " + getType() + " tile");
+    }
+}
